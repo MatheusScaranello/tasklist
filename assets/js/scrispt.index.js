03f8dbd592c4a0bd827834eca3b20286e0fe6a27
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-08-18 09:24:27 -0300

    feat: create category-product files

--- assets/js/scrispt.index.js
+++ assets/js/scrispt.index.js
@@ -1,66 +1,67 @@
-/*const numbers = [1,2,3,4,5,6,7,8,9,10];
+// /*const numbers = [1,2,3,4,5,6,7,8,9,10];
 
-console.log(numbers);
+// console.log(numbers);
 
-numbers.forEach((number, index) =>{
-    if (number % 2 == 0) {
-        console.log(`Numeros pares ${number}, sua posiçao é ${index}!`);
-    }  
-})*/
-
-
-// const cars = [
-//     {
-//         marca: "ford",
-//         modelo: "Focus",
-//     },
-//     {
-//         marca: "ferrari",
-//         modelo: "f40",
-//     },
-//     {
-//         marca: "fiat",
-//         modelo: "palio",
-//     },
-//     {
-//         marca: "BMW",
-//         modelo: "BMW Z4",
-//     },
-// ];
+// numbers.forEach((number, index) =>{
+//     if (number % 2 == 0) {
+//         console.log(`Numeros pares ${number}, sua posiçao é ${index}!`);
+//     }  
+// })*/
 
 
+// // const cars = [
+// //     {
+// //         marca: "ford",
+// //         modelo: "Focus",
+// //     },
+// //     {
+// //         marca: "ferrari",
+// //         modelo: "f40",
+// //     },
+// //     {
+// //         marca: "fiat",
+// //         modelo: "palio",
+// //     },
+// //     {
+// //         marca: "BMW",
+// //         modelo: "BMW Z4",
+// //     },
+// // ];
 
-class cars{
-    constructor(car, model){
-this.car = car,
-this.model = model;
-    }
-}
-class CarList{
-    constructor(){
-        this.cars = []
-    }
-    send(){
-        const carInput = document.getElementById('car');
-        const modelInput = document.getElementById('modelo');
-        const carText = carInput.value.trim();
-        const modelText = modelInput.value.trim();
 
-        if (carText && modelText !== '') {
-            const newCar = new cars(carText, modelText);
-            this.cars.push(newCar);
 
-            carInput.value = '';
-            modelInput.value = '';
-        }
-    }
-}
+// class cars{
+//     constructor(car, model){
+// this.car = car,
+// this.model = model;
+//     }
+// }
+// class CarList{
+//     constructor(){
+//         this.cars = []
+//     }
+//     send(){
+//         const carInput = document.getElementById('car');
+//         const modelInput = document.getElementById('modelo');
+//         const carText = carInput.value.trim();
+//         const modelText = modelInput.value.trim();
 
-const SuperCar = new CarList();
-SuperCar.cars.forEach((car, marca) => {
-    console.log(`Carro: ${car}, Marca: ${marca}`);
-})
+//         if (carText && modelText !== '') {
+//             const newCar = new cars(carText, modelText);
+//             this.cars.push(newCar);
 
-function Adicione() {
-    SuperCar.send();
-}
+//             carInput.value = '';
+//             modelInput.value = '';
+//         }
+//     }
+// }
+
+// const List = new CarList();
+// const Car = new cars();
+
+
+
+// function Adicione() {
+//     List.send();
+    
+// }
